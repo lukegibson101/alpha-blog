5f598f9f021186ab30adcb8bf36a4c6d04a9556f
lukegibson101 <luke.gibson@nourishcare.co.uk>
2022-03-28 14:25:02 +0100

    enable creation of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,17 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+        @article=Article.new
     end
 
     def create
-        @article = Article.new
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end 
     end
 
 end
